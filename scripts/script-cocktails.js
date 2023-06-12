4c546659f90478e6440a5e1a55c039be4bfc792d
Sebastian Pikand <sebastianpikand@gmail.com>
2023-06-12 16:04:39 +0300

    main

--- scripts/script-cocktails.js
+++ scripts/script-cocktails.js
@@ -1,33 +1,46 @@
 let index_of_choice = 0;
-const submit_button = document.querySelector("input[type='submit']");
+const submit_button = document.getElementById('first-submit');
 
-submit_button.addEventListener("click", (event) => {
-  event.preventDefault();
+submit_button.addEventListener('click', () => {
+  const formData = {};
 
-  if (submit_button.classList.contains("added")) {
+  const coctailValue = document.getElementById('cocktail').value;
+
+  if (coctailValue == '') {
+    alert('The input was empty! Try again!');
+    window.location.href = './cocktails.html';
+  }
+
+  formData.cocktail = coctailValue;
+
+  if (coctailValue === 'by-name') {
+    // ...
+  }
+
+  if (submit_button.classList.contains('added')) {
     const input_value = document.querySelector("input[type='text']").value;
-    if (input_value == "") {
-      alert("The input was empty! Try again!");
-      window.location.href = "./cocktails.html";
+    if (input_value == '') {
+      alert('The input was empty! Try again!');
+      window.location.href = './cocktails.html';
     } else {
       main(input_value, index_of_choice);
     }
   } else {
-    const selected_option = document.querySelector("select").value;
+    const selected_option = document.querySelector('select').value;
     if (!selected_option) {
-      alert("You must choose something from the list below before proceeding!");
-      window.location.href = "./cocktails.html";
+      alert('You must choose something from the list below before proceeding!');
+      window.location.href = './cocktails.html';
     } else {
       index_of_choice = display_additional_content(selected_option);
       if (index_of_choice === 3) {
-        main("", index_of_choice);
+        main('', index_of_choice);
       }
     }
   }
 });
 
 const main = (input_value, index_of_choice) => {
-  const loader = document.querySelector(".loader");
+  const loader = document.querySelector('.loader');
   display_loading(loader);
   const data = fetch_data(input_value, index_of_choice);
   data.then((result) => {
@@ -35,8 +48,8 @@ const main = (input_value, index_of_choice) => {
     if (validate_response(result, index_of_choice)) {
       display_response(result, index_of_choice);
     } else {
-      alert("The entered values are not in correct form! Try again!");
-      window.location.href = "./cocktails.html";
+      alert('The entered values are not in correct form! Try again!');
+      window.location.href = './cocktails.html';
     }
   });
 };
@@ -45,17 +58,11 @@ const fetch_data = async (input_value, index_of_choice) => {
   try {
     let response = null;
     if (index_of_choice === 1) {
-      response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input_value}`
-      );
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input_value}`);
     } else if (index_of_choice === 2) {
-      response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${input_value}`
-      );
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${input_value}`);
     } else if (index_of_choice === 3) {
-      response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/random.php`
-      );
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
     }
     const data = await response.json();
     return data;
@@ -65,30 +72,30 @@ const fetch_data = async (input_value, index_of_choice) => {
 };
 
 const display_additional_content = (selected_option) => {
-  if (selected_option === "by-name") {
+  if (selected_option === 'by-name') {
     additional_content("What's the name of the desired cocktail?");
     return 1;
-  } else if (selected_option === "by-ingredient") {
+  } else if (selected_option === 'by-ingredient') {
     additional_content("What's the name of the desired ingredient?");
     return 2;
-  } else if (selected_option === "by-random") {
-    submit_button.setAttribute("class", "added");
+  } else if (selected_option === 'by-random') {
+    submit_button.setAttribute('class', 'added');
     return 3;
   }
 };
 
 const additional_content = (label_text) => {
-  document.querySelector("label").remove();
-  document.querySelector("select").remove();
+  document.querySelector('label').remove();
+  document.querySelector('select').remove();
 
-  submit_button.setAttribute("class", "added");
-  const submit_link = document.querySelector("#submit");
+  submit_button.setAttribute('class', 'added');
+  const submit_link = document.querySelector('#submit');
 
-  const label = document.createElement("label");
+  const label = document.createElement('label');
   label.innerText = label_text;
   submit_link.parentNode.insertBefore(label, submit_link);
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
   submit_link.parentNode.insertBefore(input, submit_link);
 };
 
@@ -102,9 +109,9 @@ const validate_response = (response, index_of_choice) => {
 };
 
 const display_response = (data, index_of_choice) => {
-  const content = document.querySelector(".content");
-  const div = document.createElement("div");
-  div.setAttribute("class", "container");
+  const content = document.querySelector('.content');
+  const div = document.createElement('div');
+  div.setAttribute('class', 'container');
   content.append(div);
 
   let array_length = 0;
@@ -115,42 +122,42 @@ const display_response = (data, index_of_choice) => {
   }
 
   for (let i = 0; i < array_length; i++) {
-    const inner_div = document.createElement("div");
-    inner_div.setAttribute("class", "inner_container");
+    const inner_div = document.createElement('div');
+    inner_div.setAttribute('class', 'inner_container');
     div.append(inner_div);
 
     if (index_of_choice === 2) {
-      const h1_name = document.createElement("h1");
+      const h1_name = document.createElement('h1');
       h1_name.innerText = data.ingredients[i].strIngredient;
-      const p_abv = document.createElement("p");
+      const p_abv = document.createElement('p');
       p_abv.innerText = `ABV (alcohol by value): ${data.ingredients[i].strABV}`;
       inner_div.appendChild(h1_name);
       inner_div.appendChild(p_abv);
     } else {
-      const h1_name = document.createElement("h1");
+      const h1_name = document.createElement('h1');
       h1_name.innerText = data.drinks[i].strDrink;
-      const p_recipe = document.createElement("p");
+      const p_recipe = document.createElement('p');
       p_recipe.innerText = data.drinks[i].strInstructions;
       inner_div.appendChild(h1_name);
       inner_div.appendChild(p_recipe);
     }
   }
 
-  const button = document.createElement("button");
-  const a = document.createElement("a");
-  a.setAttribute("href", "./cocktails.html");
-  a.innerText = "GO AGAIN";
+  const button = document.createElement('button');
+  const a = document.createElement('a');
+  a.setAttribute('href', './cocktails.html');
+  a.innerText = 'GO AGAIN';
   button.append(a);
   div.append(button);
 };
 
 const display_loading = (loader) => {
-  const form = document.querySelector("form");
+  const form = document.querySelector('form');
   form.remove();
 
-  loader.classList.add("display");
+  loader.classList.add('display');
 };
 
 const hide_loading = (loader) => {
-  loader.classList.remove("display");
+  loader.classList.remove('display');
 };
--- scripts/script-kanye.js
+++ scripts/script-kanye.js
@@ -1,61 +1,53 @@
-const main = () => {
-  const loader = document.querySelector(".loader");
-  display_loading(loader);
-  const data = fetch_data();
-  data.then((result) => {
-    hide_loading(loader);
-    console.log(result);
-    display_response(result);
-  });
-};
-
-document.querySelector("button").addEventListener("click", main);
-
-const fetch_data = async () => {
+const main = async () => {
   try {
-    const response = await fetch("https://api.kanye.rest/");
-    const data = await response.json();
-    return data;
+    const loader = document.querySelector('.loader');
+    // toggleLoader();
+    display_loading(loader);
+    const data = await fetch('https://api.kanye.rest/').then((res) => res.json());
+    hide_loading(loader);
+    display_response(data);
   } catch (error) {
     console.log(error);
   }
 };
 
+document.querySelector('button').addEventListener('click', main);
+
 const display_response = (data) => {
-  const content = document.querySelector(".content");
-  const div = document.createElement("div");
-  div.setAttribute("class", "container");
+  const content = document.querySelector('.content');
+  const div = document.createElement('div');
+  div.setAttribute('class', 'container');
   content.append(div);
 
-  const h1 = document.createElement("h1");
+  const h1 = document.createElement('h1');
   div.append(h1);
   h1.innerHTML = `Kanye: <span>"${data.quote}!"</span>`;
 
-  const again_button = document.createElement("button");
-  again_button.setAttribute("onClick", "main()");
-  again_button.innerText = "GENERATE AGAIN";
+  const again_button = document.createElement('button');
+  again_button.setAttribute('onClick', 'main()');
+  again_button.innerText = 'GENERATE AGAIN';
   div.append(again_button);
 
-  const cancel_button = document.createElement("button");
-  const a = document.createElement("a");
-  a.setAttribute("href", "./kanye.html");
-  a.innerText = "CANCEL";
+  const cancel_button = document.createElement('button');
+  const a = document.createElement('a');
+  a.setAttribute('href', './kanye.html');
+  a.innerText = 'CANCEL';
   cancel_button.append(a);
   div.append(cancel_button);
 };
 
 const display_loading = (loader) => {
-  const content = document.querySelector(".content");
-  const form = document.querySelector("form");
-  const container = document.querySelector(".container");
+  const content = document.querySelector('.content');
+  const form = document.querySelector('form');
+  const container = document.querySelector('.container');
   if (content.contains(form)) {
     form.remove();
   } else if (content.contains(container)) {
     container.remove();
   }
-  loader.classList.add("display");
+  loader.classList.add('display');
 };
 
 const hide_loading = (loader) => {
-  loader.classList.remove("display");
+  loader.classList.remove('display');
 };
